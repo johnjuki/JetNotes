c089421510cb8a06e9694881b80923fcc3f57df0
John Njuki <njukijohn2000@gmail.com>
2022-09-01 16:23:22 +0300

    first commit

---
+++ app/src/main/java/com/raywenderlich/android/jetnotes/ui/components/Note.kt
@@ -0,0 +1,24 @@
+package com.raywenderlich.android.jetnotes.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.raywenderlich.android.jetnotes.theme.rwGreen
+
+@Composable
+fun Note() {
+    Box(modifier = Modifier
+        .size(40.dp)
+        .background(rwGreen)) {
+        Text(text = "Title", maxLines = 1)
+        Text(text = "Content", maxLines = 1)
+        Checkbox(checked = false, onCheckedChange = {}, modifier = Modifier.padding(start = 8.dp))
+    }
+}
+
